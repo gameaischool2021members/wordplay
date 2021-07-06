c0d9ab8414fd5b1417dc3f8a30c79c77bdaef9b9
KOSTANTINOS BANAKAKIS <mpanakak@hotmail.com>
2021-07-06 20:19:32 +0300

    Unity Project
    Unity Project

---
+++ Summer School/Assets/Scripts/RoomBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBehaviour : MonoBehaviour
+{
+    [SerializeField] GameObject northWall;
+    [SerializeField] GameObject eastWall;
+    [SerializeField] GameObject southWall;
+    [SerializeField] GameObject westWall;
+
+}
